9cc4dd2adb01a8b8c4c45fbeb77a17b0f0ec8b2a
Francis Isip <isipf908@gmail.com>
2026-05-05 15:57:24 -0700

    hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print("Git is awesome!")
+#Hello! Check this code out!
+
+name = input("Enter your name pretty please: ")
+age = input("Plssss enter your age: ")
+
+
+
+print("MY NAME is {} and I AM {} years old!!!". format(name, age))
+print("Hello World!!")
